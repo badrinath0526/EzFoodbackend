cdf15621f03534dc06eba1bcbf348162832a50f2
unknown <badrinathchowdary@gmail.com>
2024-03-20 18:24:26 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3005;
+const port = process.env.PORT||3005;
 const mongoDb = require("./db");
 
 mongoDb();
